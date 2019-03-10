5e450910b424bd90000eb4ed19c8e48b766f2a5d
Thiago <thiagosato22@hotmail.com>
2019-03-10 09:45:04 -0300

    Add get frame in class Camera

--- system/camera/camera.py
+++ system/camera/camera.py
@@ -4,10 +4,11 @@ import cv2 as cv
 class Camera:
     def __init__(self):
         self.video = cv.VideoCapture(0)
+        _, self.frame = self.video.read()
 
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
-        ret, image = self.video.read()
-        return image
+        _, self.frame = self.video.read()
+        return self.frame

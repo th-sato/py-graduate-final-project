de8db6fe32e3a7c004f98bc65e507d17c61f9565
Thiago <thiagosato22@hotmail.com>
2019-03-10 09:49:22 -0300

    Add constants

---
+++ system/constants/constants.py
@@ -0,0 +1,19 @@
+# Image
+HTML_IMAGE_HEADER = "data:image/png;base64, "
+
+# Image dimensions
+# WIDTH_IMAGE = 640  # Pixels
+# HEIGHT_IMAGE = 480  # Pixels
+
+# Lane dimension
+WIDTH_LANE = 22.0  # Centimeters
+
+# Measures
+axis_x_meters_per_pixel = 30.0/720
+axis_y_meters_per_pixel = 3.7/700
+
+# Colors
+WHITE = 255
+BLACK = 0
+BLUE = (225, 100, 60)
+GREEN = (0, 255, 0)

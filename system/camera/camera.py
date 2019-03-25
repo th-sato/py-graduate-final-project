a80158cfed28f61581c125bf988de9c30cd0280a
Thiago <thiagosato22@hotmail.com>
2019-03-25 02:50:56 +0000

    Resolução da câmera

--- system/camera/camera.py
+++ system/camera/camera.py
@@ -4,6 +4,12 @@ import cv2 as cv
 class Camera:
     def __init__(self):
         self.video = cv.VideoCapture(0)
+        self.video.set(cv.cv.CV_CAP_PROP_FRAME_WIDTH, 432.)
+	self.video.set(cv.cv.CV_CAP_PROP_FRAME_HEIGHT, 240.)
+#	self.video.set(cv.cv.CV_CAP_PROP_FRAME_WIDTH, 432.)
+#	self.video.set(cv.cv.CV_CAP_PROP_FRAME_HEIGHT, 240.)
+#       self.video.set(cv.cv.CV_CAP_PROP_FRAME_WIDTH, 1920.)
+#       self.video.set(cv.cv.CV_CAP_PROP_FRAME_HEIGHT, 1200.)
         _, self.frame = self.video.read()
 
     def __del__(self):

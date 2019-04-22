cbbd2c6733512fcb635546bec251b051e53692a3
Thiago <thiagosato22@hotmail.com>
2019-04-21 22:42:19 -0300

    setRequestHeader(Content-Type, application/json)

--- web-application/static/js/home.js
+++ web-application/static/js/home.js
@@ -32,6 +32,7 @@ function requestPostHttp(path, json){
     var protocol  = window.location.protocol;
     url = HOST_AUTONOMOUS_CAR + "/" + path;
     xmlHttp.open("POST", url, true);
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
     xmlHttp.send(json);
     return xmlHttp.responseText;
 }

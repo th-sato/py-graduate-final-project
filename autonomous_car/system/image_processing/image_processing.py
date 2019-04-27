f77917bae0b660714ed12b7a03c7009932544074
Thiago <thiagosato22@hotmail.com>
2019-04-27 16:47:55 -0300

    Teste: apresentando valor obtido pela câmera

--- autonomous_car/system/image_processing/image_processing.py
+++ autonomous_car/system/image_processing/image_processing.py
@@ -144,7 +144,7 @@ def curvature(left_fit, right_fit, binary_warped):
     lane_center = (left_lane_bottom + right_lane_bottom) / 2.
     distance_center = (lane_center - center_image) * xm_per_pix  # Convert to meters
 
-    print "Y: ", plot_y, ". left_lane_bottom: ", left_lane_bottom, ". right_lane_bottom: ", right_lane_bottom
+    print "Y: ", y_eval, ". left_lane_bottom: ", left_lane_bottom, ". right_lane_bottom: ", right_lane_bottom
     print "lane_center: ", lane_center, ". center_image:", center_image, ". distance_center: ", distance_center
 
     # Identify new coefficients in meters

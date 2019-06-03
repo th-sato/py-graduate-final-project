ec14cb70a9f5a12ec959cb823d72a900cf1a2f96
Thiago <thiagosato22@hotmail.com>
2019-06-02 23:05:19 -0300

    Changing fourcc

--- autonomous_car/system/image_processing/image_processing.py
+++ autonomous_car/system/image_processing/image_processing.py
@@ -10,7 +10,7 @@ LOCAL_PATH = os.path.dirname(__file__)  # get current directory
 
 def video_writer():
     # fourcc = cv.VideoWriter_fourcc(*'H264')
-    fourcc = 0x7634706d
+    fourcc = 0x00000020
     return cv.VideoWriter(VIDEO_NAME, fourcc, 20.0, (WIDTH_IMAGE, HEIGHT_IMAGE))
 
 

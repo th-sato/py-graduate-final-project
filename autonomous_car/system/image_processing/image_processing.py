2a0d6196012341d5d25610ce595c40bf61fd87bf
Thiago <thiagosato22@hotmail.com>
2019-06-02 22:52:11 -0300

    Fixing video coded

--- autonomous_car/system/image_processing/image_processing.py
+++ autonomous_car/system/image_processing/image_processing.py
@@ -9,7 +9,7 @@ LOCAL_PATH = os.path.dirname(__file__)  # get current directory
 
 
 def video_writer():
-    fourcc = cv.VideoWriter_fourcc(*'MP4V')
+    fourcc = cv.VideoWriter_fourcc(*'H264')
     return cv.VideoWriter(VIDEO_NAME, fourcc, 20.0, (WIDTH_IMAGE, HEIGHT_IMAGE))
 
 

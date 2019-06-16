d013e34e06bb29f1c09ca10bae2afdaaf627d7d7
Thiago <thiagosato22@hotmail.com>
2019-06-15 21:33:26 -0300

    Fixing: changing name of attr

--- autonomous_car/logs/log.py
+++ autonomous_car/logs/log.py
@@ -3,14 +3,14 @@ from redis_communication import RedisCommunication
 
 class Log:
     def __init__(self):
-        self._redis = RedisCommunication()
+        self._redis_communication = RedisCommunication()
         self._key = "log_car"
 
     def clean_log(self):
-        self._redis.delete_values_of_key(self._key)
+        self._redis_communication.delete_values_of_key(self._key)
 
     def store_object(self, item):
-        self._redis.add_value_by_key(self._key, item)
+        self._redis_communication.add_value_by_key(self._key, item)
 
     def get_log(self):
-        self._redis.get_values_by_key(self._key)
+        self._redis_communication.get_values_by_key(self._key)

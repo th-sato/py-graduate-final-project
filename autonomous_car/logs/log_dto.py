784f8a5124d498d6255df0ed7d209ec87df7ed02
Thiago <thiagosato22@hotmail.com>
2019-06-15 22:26:20 -0300

    Fixing: saving object the other way

--- autonomous_car/logs/log_dto.py
+++ autonomous_car/logs/log_dto.py
@@ -40,5 +40,5 @@ class LogDto:
         self._curv = new_curv
 
     def __repr__(self):
-        return "'speed': '", self._speed, "', 'angle': '", self._angle, "', 'dist_center': '", self._dist_center,\
-               "', 'curv': '", self._curv, "'"
+        return "'speed': '" + self._speed + "', 'angle': '" + self._angle + "', 'dist_center': '" + self._dist_center\
+               + "', 'curv': '" + self._curv, "'"

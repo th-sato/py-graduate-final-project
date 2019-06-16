7c082c1319c6afab8c4764391a04601f456245c0
Thiago <thiagosato22@hotmail.com>
2019-06-15 22:50:09 -0300

    Testing: get_log

--- autonomous_car/logs/redis_communication.py
+++ autonomous_car/logs/redis_communication.py
@@ -13,6 +13,4 @@ class RedisCommunication:
         self.connection.delete(key)
 
     def get_values_by_key(self, key):
-        print "key", key
-        print self.connection.smembers(key)
         return self.connection.smembers(key)
